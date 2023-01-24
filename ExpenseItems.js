d19959e4fbe509cb7374207af6a69a8cc1702920
tej00001 <119431283+tej00001@users.noreply.github.com>
2023-01-24 21:10:50 +0530

    adding expenseitems

---
+++ ExpenseItems.js
@@ -0,0 +1,13 @@
+function ExpenseItems() {
+  return (
+    <div>
+      <p>
+        <b>Expense Items</b>
+        <div>Food 100 Rs</div>
+        <div>Petrol 100 Rs</div>
+        <div>Movie 100 Rs</div>
+      </p>
+    </div>
+  );
+}
+export default ExpenseItems;
