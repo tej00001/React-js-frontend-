79ea639dd1c4bd045e226c8950245f041e7e3735
tej00001 <tejachintu207@gmail.com>
2023-01-26 16:06:18 +0530

    adding task11

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,20 +1,36 @@
 import "./ExpenseItems.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
+import Card from "./Card";
 
 function ExpenseItems(props) {
   return (
-    <div className="expense-item">
-      <ExpenseDate date={props.data.date}></ExpenseDate>
-      <ExpenseDetails
-        amount={props.data.amount}
-        LocationOfExpenditure={props.data.LocationOfExpenditure}
-      ></ExpenseDetails>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.data.name}</h2>
+        <h2>{props.name}</h2>
+        <ExpenseDetails
+          amount={props.amount}
+          LocationOfExpenditure={props.LocationOfExpenditure}
+        ></ExpenseDetails>
       </div>
-    </div>
+    </Card>
   );
 }
+
+// function ExpenseItems(props) {
+//   return (
+//     <div className="expense-item">
+//       <ExpenseDate date={props.data.date}></ExpenseDate>
+//       <ExpenseDetails
+//         amount={props.data.amount}
+//         LocationOfExpenditure={props.data.LocationOfExpenditure}
+//       ></ExpenseDetails>
+//       <div className="expense-item__description">
+//         <h2>{props.data.name}</h2>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default ExpenseItems;
